97dd90a083bfff0aac457f8fab5da5037b85bafc
Светослав Балтаджиев <baltadjiev@mail.bg>
2024-04-11 22:56:43 +0300

    Removed EMAIL SENDER on registration

--- Best_Rent-A-Car/Best_Rent-A-Car/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Best_Rent-A-Car/Best_Rent-A-Car/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -23,18 +23,16 @@ namespace Best_Rent_A_Car.Areas.Identity.Pages.Account
         private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
 
         public RegisterModel(
             UserManager<User> userManager,
             SignInManager<User> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            ILogger<RegisterModel> logger
+            )
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
         }
 
         [BindProperty]
@@ -88,9 +86,6 @@ namespace Best_Rent_A_Car.Areas.Identity.Pages.Account
                         pageHandler: null,
                         values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
