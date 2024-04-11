21987951c30b628edf5a8dffcbc3380134a41e7b
Светослав Балтаджиев <baltadjiev@mail.bg>
2024-04-11 20:38:51 +0300

    Project Creation
    Initial

---
+++ Best_Rent-A-Car/Best_Rent-A-Car/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Best_Rent_A_Car.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
