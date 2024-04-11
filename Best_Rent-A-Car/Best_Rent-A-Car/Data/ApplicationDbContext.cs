fc2679488b600f1e18804159a0b50c41124cf963
Кристиян Джургов <krisi100105djurgov@gmail.com>
2024-04-11 21:05:35 +0300

    Model Car; CarsController
    Created Car Model, Created CarsController, Auto Views Generated

--- Best_Rent-A-Car/Best_Rent-A-Car/Data/ApplicationDbContext.cs
+++ Best_Rent-A-Car/Best_Rent-A-Car/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Best_Rent_A_Car.Models;
 
 namespace Best_Rent_A_Car.Data
 {
@@ -12,5 +13,6 @@ namespace Best_Rent_A_Car.Data
             : base(options)
         {
         }
+        public DbSet<Best_Rent_A_Car.Models.Car> Car { get; set; }
     }
 }
