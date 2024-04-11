4738ca6898cb637bbab35cb65e583231530550d2
Светослав Балтаджиев <baltadjiev@mail.bg>
2024-04-11 21:21:15 +0300

    Model CarReservation
    Created model CarReservation

---
+++ Best_Rent-A-Car/Best_Rent-A-Car/Models/CarReservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Best_Rent_A_Car.Models
+{
+    public class CarReservation
+    {
+        public int ID { get; set; }
+        public int CarID { get; set; }
+        public Car Car { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+    }
+}
