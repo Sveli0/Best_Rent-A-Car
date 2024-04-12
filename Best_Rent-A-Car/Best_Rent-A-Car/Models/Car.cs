845279b4e40aaab468152272b1dc202d6d90f242
atatemur <atatemur@abv.bg>
2024-04-12 18:47:26 +0300

    Added validation for Car.cs

--- Best_Rent-A-Car/Best_Rent-A-Car/Models/Car.cs
+++ Best_Rent-A-Car/Best_Rent-A-Car/Models/Car.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Best_Rent_A_Car.Models.Attributes;
 
 namespace Best_Rent_A_Car.Models
 {
     public class Car
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string Brand { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string Model { get; set; }
+        [Display(Name ="Year:")]
+        [DataType(DataType.Date)]
+        [YearAttribute(ErrorMessage = "Time cannot be set past the present.")]
         public DateTime Year { get; set; }
+        [Required]
+        [Display(Name = "Number of seats:")]
+        [Range(0, int.MaxValue, ErrorMessage ="Seats cannot be a negtaive number.")]
         public int Seats { get; set; }
+        [Display(Name = "Info:")]
         public string Info { get; set; }
+        [Required]
+        [Display(Name ="Price per day:")]
+        [Range(0, double.MaxValue, ErrorMessage ="Price per day cannot be negative.")]
         public double PricePerDay { get; set; }
         public Car()
         {
