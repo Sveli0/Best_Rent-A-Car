0ce5e8547474d29ac59b32a908d3e28b7e82d26c
Светослав Балтаджиев <baltadjiev@mail.bg>
2024-04-13 23:42:52 +0300

    Update Email.cshtml.cs

--- Best_Rent-A-Car/Best_Rent-A-Car/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ Best_Rent-A-Car/Best_Rent-A-Car/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -92,20 +92,10 @@ namespace Best_Rent_A_Car.Areas.Identity.Pages.Account.Manage
             var email = await _userManager.GetEmailAsync(user);
             if (Input.NewEmail != email)
             {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmailChange",
-                    pageHandler: null,
-                    values: new { userId = userId, email = Input.NewEmail, code = code },
-                    protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(
-                    Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                var token = _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
+                await _userManager.ChangeEmailAsync(user, Input.NewEmail, token);
 
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                StatusMessage = "Successfully changed email.";
                 return RedirectToPage();
             }
 
