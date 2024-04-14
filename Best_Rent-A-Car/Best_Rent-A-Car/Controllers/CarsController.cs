5e7f4d4616a71c720742de37185f220c946d993d
atatemur <atatemur@abv.bg>
2024-04-14 03:25:50 +0300

    Comments in usercontroller and car controller, fix in search method

--- Best_Rent-A-Car/Best_Rent-A-Car/Controllers/CarsController.cs
+++ Best_Rent-A-Car/Best_Rent-A-Car/Controllers/CarsController.cs
@@ -10,20 +10,30 @@ namespace Best_Rent_A_Car.Controllers
 {
     public class CarsController : Controller
     {
+        /// <summary>
+        /// This is an mvc class mostly generated through the wonders of razor and asp, still it hold some great methods
+        /// </summary>
         private readonly ApplicationDbContext _context;
 
         public CarsController(ApplicationDbContext context)
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Returns a collection of cars to the Index View so it can show them properly
+        /// </summary>
+        /// <returns></returns>
         // GET: Cars
         [Authorize]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Cars.ToListAsync());
         }
-
+        /// <summary>
+        /// This is an HttpGet method, Gets the details of a car through a provided id and gives it to the detail view
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // GET: Cars/Details/5
         [Authorize]
         public async Task<IActionResult> Details(int? id)
@@ -42,14 +52,21 @@ namespace Best_Rent_A_Car.Controllers
 
             return View(car);
         }
-
+        /// <summary>
+        /// The HttpGet method that invokes the create View
+        /// </summary>
+        /// <returns></returns>
         // GET: Cars/Create
         [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             return View();
         }
-
+        /// <summary>
+        /// The HttpPost action for creating cars, it binds the inputs to the appropriate fields
+        /// </summary>
+        /// <param name="car"></param>
+        /// <returns></returns>
         // POST: Cars/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -66,7 +83,11 @@ namespace Best_Rent_A_Car.Controllers
             }
             return View(car);
         }
-
+        /// <summary>
+        /// HttpGet for the Edit action, it supplies the view with the details it needs
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // GET: Cars/Edit/5
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int? id)
@@ -83,7 +104,12 @@ namespace Best_Rent_A_Car.Controllers
             }
             return View(car);
         }
-
+        /// <summary>
+        /// The HttpPost action for the Edit, it takes the car and updates it within the context, with the new Binded fields
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="car"></param>
+        /// <returns></returns>
         // POST: Cars/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -118,7 +144,11 @@ namespace Best_Rent_A_Car.Controllers
             }
             return View(car);
         }
-
+        /// <summary>
+        /// The HttpGet for the DeleteView, it supplies the deletConfirm view with the necessary details
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // GET: Cars/Delete/5
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete(int? id)
